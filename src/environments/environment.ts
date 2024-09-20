ade9c9c7419e1e57ce59a140638c16a120bb52e8
Konstantinos Gatis <kgatis@iknowhow.com>
2024-09-20 16:59:10 +0300

    change api url for prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://analytics-service-zrfe.onrender.com/',
+  baseUrl: 'https://galen-service-cpase6gzavacgwee.eastus-01.azurewebsites.net/',
   label:'prod',
-  SOCKET_ENDPOINT: 'https://analytics-service-zrfe.onrender.com/'
+  SOCKET_ENDPOINT: 'https://galen-service-cpase6gzavacgwee.eastus-01.azurewebsites.net/'
 };
